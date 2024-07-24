347f30c3edac64756919484160902729705d0216
ANAS <anaspappadan@gmail.com>
2024-07-24 14:43:26 +0530

    feat: install browser router and create routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,19 @@
+import Home from "./pages/Home";
 import SliderPage from "./pages/SliderPage";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <SliderPage />
+      <BrowserRouter>
+        <button>
+          <Link to="/parallaxslider">Image Slider With Parallax Effect</Link>
+        </button>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/parallaxslider" element={<SliderPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
