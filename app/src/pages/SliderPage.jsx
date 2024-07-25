2a36631be94768913e82475430ce2335380b25f3
ANAS <anaspappadan@gmail.com>
2024-07-25 13:20:59 +0530

    update the slider page

--- app/src/pages/SliderPage.jsx
+++ app/src/pages/SliderPage.jsx
@@ -4,14 +4,12 @@ import Slider from "../components/pure-css/slider/Slider";
 export default function SliderPage() {
   return (
     <div style={{ overflow: "hidden" }}>
-      <h2>COOL IMAGE SLIDER</h2>
+      <h2>PARALLAX IMAGE SLIDER</h2>
       <div>
         <Slider images={imagesColored} />
       </div>
+      <br />
       <div>
-        <h3 style={{ textAlign: "center" }}>
-          Images with black background look more cool..!!
-        </h3>
         <Slider images={imagesBlack} />
       </div>
     </div>
